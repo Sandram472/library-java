afafcb04ef7452fd9683da8a27c7754a2b568a00
sandramartinez <alvmart2010@gmail.com>
2026-06-02 16:08:40 -0500

    Se crean funciones en service para consultar por isbn, titulo y catalogo completo

--- src/main/java/co/com/uniremington/service/Library.java
+++ src/main/java/co/com/uniremington/service/Library.java
@@ -5,26 +5,50 @@ import co.com.uniremington.model.Loan;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Library {
-    List<Book> bookList = new ArrayList<>();
-    List<Loan> loanList = new ArrayList<>();
+    private List<Book> bookList = new ArrayList<Book>();
+    private List<Loan> loanList = new ArrayList<>();
 
-    public boolean addBook (String isbn, String title, String author, String category){
-        boolean bookExists = false;
+    public boolean addBook(String isbn, String title, String author, String category) {
 
-        for (int i = 0; i < this.bookList.size(); i++){
-            if (this.bookList.get(i).getIsbn()== isbn){
-               bookExists = true;
-               break;
+        if (this.findBookByIsbn(isbn) != null) {
+            return false;
+        } else {
+
+            Book newBook = new Book(isbn, title, author, category);
+            this.bookList.add(newBook);
+            return true;
+        }
+
+    }
+
+    public Book findBookByIsbn(String isbn) {
+        Book book = null;
+        for (int i = 0; i < this.bookList.size(); i++) {
+            if (Objects.equals(this.bookList.get(i).getIsbn(), isbn)) {
+                book = this.bookList.get(i);
+                return book;
             }
         }
-        if (bookExists){
-            return false;
-        }
-        Book newBook = new Book(isbn, title, author, category);
-        this.bookList.add(newBook);
-        return true;
+        return book;
 
+    }
+
+    public List<Book> findBookByTitle(String title) {
+
+        List<Book> books = new ArrayList<>();
+
+        for (int i = 0; i < this.bookList.size(); i++) {
+            if (this.bookList.get(i).getTitle().toLowerCase().contains(title.toLowerCase())) {
+                books.add(this.bookList.get(i));
+            }
+        }
+        return books;
+    }
+
+    public List<Book> getCatalog() {
+        return this.bookList;
     }
 }
